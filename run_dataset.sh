bce7dd4f4a6f1c96d0e7bbb526284fe4993707f4
ZVengin <wenjiezhong717@gmail.com>
2025-07-10 20:39:50 +0900

    add files

---
+++ run_dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module load singularity
+
+code_dir=/work/gk77/k77006/repos/writing_mode_project
+#repos_dir=~/repos
+magictools=/work/gk77/k77006/repos/speaker_identification_project/MagicTools
+
+singularity shell --nv --home ${code_dir}/container/kelvin --workdir ${code_dir} --bind ${code_dir}:/tmp/code,${magictools}:/tmp/magictools  ${code_dir}/style_project.sif
