ad72b85b77e6ed715b843190c6676c07743e8c8f
ZVengin <wenjiezhong717@gmail.com>
2025-07-24 17:01:36 +0900

    add files

--- run_dataset.sh
+++ run_dataset.sh
@@ -6,4 +6,4 @@ code_dir=/work/gk77/k77006/repos/writing_mode_project
 #repos_dir=~/repos
 magictools=/work/gk77/k77006/repos/speaker_identification_project/MagicTools
 
-singularity shell --nv --home ${code_dir}/container/kelvin --workdir ${code_dir} --bind ${code_dir}:/tmp/code,${magictools}:/tmp/magictools  ${code_dir}/style_project.sif
+singularity shell --nv --home ${code_dir}/container/kelvin --workdir ${code_dir} --bind ${code_dir}:/tmp/code,${magictools}:/tmp/magictools  ${code_dir}/python-313-amd64.sif
