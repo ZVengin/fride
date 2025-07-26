70055a369b2991e3acdfe48a169d6d4961e5a3a8
ZVengin <wenjiezhong717@gmail.com>
2025-07-26 19:58:34 +0900

    add files

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PJM -L rscgrp=share-debug
+#PJM -L rscgrp=share-short
 #PJM -L jobenv=singularity
 #PJM -g gk77
 #PJM -j
-#PJM -L elapse=00:30:00
+#PJM -L elapse=08:00:00
 #PJM -L gpu=1
 #PJM -N eval_gpt
 #PJM -o run_eval_gpt_split_0
@@ -17,7 +17,7 @@ convert_format="python epub2json.py \
 annotate_mode="python annotate_llm_eval_book.py \
   --sour_dir data/smashwords_json \
   --target_dir data/smashwords_wmode \
-  --model_checkpoint /tmp/code/writing_mode_classifier"
+  --model_checkpoint /tmp/code/writing_mode_classifier/model_checkpoint"
 
 create_eval_set="python create_eval_dataset.py \
   --sour_dir data/smashwords_wmode \
@@ -25,5 +25,5 @@ create_eval_set="python create_eval_dataset.py \
 
 code_dir=$(pwd)
 
-#singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir} --bind ${code_dir}:/tmp/code  ${code_dir}/python-313-amd64.sif bash -c "export WANDB_DISABLED=true && cd /tmp/code/llm_eval && ${convert_format} && ${annotate_mode}"
+singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir} --bind ${code_dir}:/tmp/code  ${code_dir}/python-313-amd64.sif bash -c "export WANDB_DISABLED=true && cd /tmp/code/llm_eval && ${convert_format} && ${annotate_mode}"
 singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir} --bind ${code_dir}:/tmp/code  ${code_dir}/python-313-amd64.sif bash -c "export WANDB_DISABLED=true && cd /tmp/code/llm_eval && ${create_eval_set}"
